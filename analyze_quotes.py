caad070a334b55fb5426533b248d605c42a2b6ea
prakash-r3ddy <prakashreddy.eega@gmail.com>
2022-08-14 12:45:27 +0530

    new comit

--- analyze_quotes.py
+++ analyze_quotes.py
@@ -1,40 +1,29 @@
 import sqlite3
 from unittest import result
 
-conn = sqlite3.connect("quotes.db")
-cursor_obj = conn.cursor()
-
-
 #1. return total no of quotations on the wbsite
 
-def get_total_no_of_quotations():
+def get_total_no_of_quotations(cursor_obj):
     cursor_obj.execute(""" SELECT COUNT(quote_id) FROM quotes""")
     result = cursor_obj.fetchall()
     count, = result[0]
     print("Totl no of quotations: "+str(count))
 
-get_total_no_of_quotations()
-
- 
 #2. total quotation by author
 
-def get_no_of_quotes_by_author():
+def get_no_of_quotes_by_author(cursor_obj):
     user_input = input("ENTER AUTHOR NAME: ")
     author_name = (user_input,)
-    
     cursor_obj.execute(""" SELECT COUNT(quote_id) FROM quotes INNER JOIN
                                 authors on quotes.author_id = authors.author_id WHERE authors.author LIKE ? """, author_name)
     result = cursor_obj.fetchall()
     total_quotes, = result[0]
-
     print("Total Quotations by "+user_input+" are: "+str(total_quotes))
-get_no_of_quotes_by_author()
-
 
 #3.Return Minimum, Maximum, and Average no. of tags on the quotations
 
-def get_min_max_avg_no_of_tags_on_quotatins():
-    cursor_obj.execute(""" SELECT MIN(no_of_tags_used), MAX(no_of_tags_used), avg(no_of_tags_used) FROM quotes """)
+def get_min_max_avg_no_of_tags_on_quotatins(cursor_obj):
+    cursor_obj.execute(""" SELECT MIN(no_of_tags_used),MAX(no_of_tags_used),AVG(no_of_tags_used) FROM quotes """)
     results = cursor_obj.fetchall()
     results = results[0]
     (min_tags, max_tags, avg_of_tags) = results
@@ -43,12 +32,9 @@ def get_min_max_avg_no_of_tags_on_quotatins():
     avg = "  avg of tags: "+str(avg_of_tags)
     print(min,max,avg)
 
-get_min_max_avg_no_of_tags_on_quotatins()
-
-
 #4.Given a number N return top N authors who authored the maximum number of quotations sorted in descending order of no. of quotes
 
-def get_who_authored_max_num_of_quotations():
+def get_who_authored_max_num_of_quotations(cursor_obj):
     user_input = int(input("ENTER NUMBER: "))
     number = (user_input,)
     cursor_obj.execute(""" SELECT author, COUNT(author) AS no_of_quotes FROM authors INNER JOIN quotes ON 
@@ -56,5 +42,12 @@ def get_who_authored_max_num_of_quotations():
     result = cursor_obj.fetchall()
     print(result)
 
-get_who_authored_max_num_of_quotations()
-
+def get_answers_for_given_questions():
+    conn = sqlite3.connect("quotes.db")
+    cursor_obj = conn.cursor()
+    get_total_no_of_quotations(cursor_obj)
+    get_no_of_quotes_by_author(cursor_obj)
+    get_min_max_avg_no_of_tags_on_quotatins(cursor_obj)
+    get_who_authored_max_num_of_quotations(cursor_obj)
+""
+get_answers_for_given_questions()
--- store_quotes.py
+++ store_quotes.py
@@ -4,81 +4,104 @@ import json
 import sqlite3
 from unittest import result
 
-def insert_values_into_quotes_table(cursor_obj,conn,quotes_data):
-    length = len(quotes_data["quotes"])
+def get_auther_id_from_authors_table(author_name,cursor_obj,conn):
+    cursor_obj.execute('SELECT author_id FROM authors WHERE author = ? ',author_name)
+    author_id = cursor_obj.fetchall()
+    author_id, = (author_id[0])
+    return author_id
+
+def insert_values_into_quotes_table(cursor_obj,conn,quotes_list):
+    length = len(quotes_list)
     id = 0
     for i in range(length):
-        quote_list = quotes_data["quotes"][i]
-        quote_obj = dict(quote_list)
+        quote_obj = quotes_list[i]
         quote = quote_obj["quote"]
+        tags_list = quote_obj["tags"]
         author_name = (quote_obj["author"],)
-        tags_count = len(quote_obj["tags"])
-        cursor_obj.execute('SELECT author_id FROM authors WHERE author = ? ',author_name)
-        author_id = cursor_obj.fetchall()
-        author_id, = (author_id[0])
+        author_id = get_auther_id_from_authors_table(author_name,cursor_obj,conn)
         id += 1
-        cursor_obj.execute("""INSERT INTO quotes VALUES(?,?,?,?)""",(id, quote,tags_count,author_id))
+        cursor_obj.execute("""INSERT INTO quotes VALUES(?,?,?,?)""",(id, quote,len(tags_list),author_id))
     conn.commit()  
       
-def insert_values_into_author_table(cursor_obj,conn,quotes_data):
-    unique_authors_list = []
-    for i in quotes_data["authors"]:
-        if i not in unique_authors_list:
-            unique_authors_list.append(i)
-    length = len(unique_authors_list)
+def insert_values_into_author_table(cursor_obj,conn,authors_list):
+    length = len(authors_list)
     id = 0
     for i in range(length):
-        author_list = unique_authors_list[i]
-        author_obj = dict(author_list)
-        author_name = author_obj["name"]
-        born_details = author_obj["born"]
-        refenrence_link = author_obj["reference"]
+        author_obj = authors_list[i]
+        author_name, born_details, refenrence_link = author_obj["name"],author_obj["born"],author_obj["reference"]
         id +=1
         cursor_obj.execute("""INSERT INTO authors VALUES(?,?,?,?)""",(id,author_name,born_details, refenrence_link))
     conn.commit()    
+
+def get_all_tags_from_each_quote(quotes_list):
+    tags_list = []
+    for i in range(len(quotes_list)):
+        tags_list.extend(quotes_list[i]["tags"])
+    return tags_list   
+ 
+def get_unique_tags_from_each_quote(quotes_list):
+    unique_tags_list = []
+    tags_list = get_all_tags_from_each_quote(quotes_list)
+    for each_tag in tags_list:
+        if each_tag not in unique_tags_list:
+            unique_tags_list.append(each_tag)  
+    return unique_tags_list    
     
-def insert_values_into_tags_table(cursor_obj,conn,quotes_data):
-    length = len(quotes_data["quotes"])
+def insert_values_into_tags_table(cursor_obj,conn,quotes_list):
+    unique_tags_list = get_unique_tags_from_each_quote(quotes_list)
     id = 0
-    for i in range(length):
-        quote = quotes_data["quotes"][i]
-        quote_obj = dict(quote)
-        quote_tags = quote_obj["tags"]
-        tags_count = len(quote_tags)
+    for each_tag in unique_tags_list:
         id += 1
-        for j in range(tags_count):
-            tag = quote_tags[j]
-            print(id,tag,id)
-            cursor_obj.execute("""INSERT INTO tags VALUES(?,?,?)""",(id,tag,id))    
+        cursor_obj.execute("""INSERT INTO tags VALUES(?,?)""",(id,each_tag))    
     conn.commit()    
 
 def create_tables(cursor_obj):
     cursor_obj.execute(""" CREATE TABLE quotes(quote_id INT NOT NULL PRIMARY KEY, quote TEXT,no_of_tags_used INT, author_id INT,FOREIGN KEY (author_id) REFERENCES authors(author_id) ON DELETE CASCADE )""")
     cursor_obj.execute(""" CREATE TABLE authors(author_id INT NOT NULL PRIMARY KEY,author TEXT, born TEXT, reference TEXT)""")
-    cursor_obj.execute(""" CREATE TABLE tags(tag_id INT,tag TEXT, quote_id INT, FOREIGN KEY (quote_id) REFERENCES quotes(quote_id) ON DELETE CASCADE  )""")
+    cursor_obj.execute(""" CREATE TABLE tags(tag_id INT NOT NULL PRIMARY KEY,tag TEXT)""")
+    cursor_obj.execute(""" CREATE TABLE quote_tags(quote_id INT,tag_id INT, FOREIGN KEY(tag_id) REFERENCES tags(tag_id),
+                                                                    FOREIGN KEY(quote_id) REFERENCES quotes(quote_id) ) """)
+        
+    
+def get_tag_id_and_insert_into_qeote_tags_table(quote_id,cursor_obj,tags_list):
+    for each_tag in tags_list:
+        tag_name = (each_tag,)
+        cursor_obj.execute('SELECT tag_id FROM tags WHERE tag = ? ',tag_name)
+        tag_id = cursor_obj.fetchall()
+        tag_id, = (tag_id[0])
+        cursor_obj.execute(""" INSERT INTO quote_tags VALUES(?,?)""",(quote_id,tag_id))
+        
+    
+def insert_values_into_quote_tag_table(cursor_obj,conn,quotes_list):
+    id = 0
+    for i in range(len(quotes_list)):
+        id += 1
+        tags_list = quotes_list[i]["tags"]
+        if len(tags_list) > 0:
+            get_tag_id_and_insert_into_qeote_tags_table(id,cursor_obj,tags_list)
+    conn.commit()        
 
-
-def insert_values_into_tables(cursor_obj,conn,quotes_data):
-    insert_values_into_quotes_table(cursor_obj,conn,quotes_data)
-    insert_values_into_author_table(cursor_obj,conn,quotes_data)
-    insert_values_into_tags_table(cursor_obj,conn,quotes_data)
-    return
+def insert_values_into_tables(cursor_obj,conn,quotes_and_authors_obj):
+    insert_values_into_quotes_table(cursor_obj,conn,quotes_and_authors_obj["quotes"])
+    insert_values_into_author_table(cursor_obj,conn,quotes_and_authors_obj["authors"])
+    insert_values_into_tags_table(cursor_obj,conn,quotes_and_authors_obj["quotes"])
+    insert_values_into_quote_tag_table(cursor_obj,conn,quotes_and_authors_obj["quotes"])
+    
 
 def create_insert_values_into_tables():
     file_name = "quotes.json"
     with open(file_name,"r", encoding="utf-8") as f:
-        data=json.load(f)
-    quotes_data=json.loads(data) 
-    authors = quotes_data["authors"]
-    
+        quotes_and_authors_obj =json.load(f) 
+    authors = quotes_and_authors_obj["authors"]
     conn = sqlite3.connect("quotes.db")
-    cursor_obj = conn.cursor()
-      
+    cursor_obj = conn.cursor()  
     #create_tables(cursor_obj)
-    #insert_values_into_tables(cursor_obj,conn,quotes_data)
+    #insert_values_into_tables(cursor_obj,conn,quotes_and_authors_obj)
+    
+    
     
     #cursor_obj.execute("""DROP TABLE quotes""")
-    #cursor_obj.execute("""SELECT count(*) AS total FROM tags """)
+    #cursor_obj.execute("""SELECT (count(quote_id)) as p FROM quote_tags  GROUP BY quote_ida """)
     #results = cursor_obj.fetchall()
     #print(results)
 
